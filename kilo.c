da852cc76e376281fe06ebb6ca3aba75318c16c0
Robert Williams <robert@increscent.org>
2021-01-17 00:40:56 -0600

    updated position

--- kilo.c
+++ kilo.c
@@ -1,5 +1,5 @@
 // DONATE WHEN I'M DONE
-// https://viewsourcecode.org/snaptoken/kilo/04.aTextViewer.html
+// https://viewsourcecode.org/snaptoken/kilo/04.aTextViewer.html#vertical-scrolling
 
 #include <ctype.h>
 #include <errno.h>
